ef3fe64975f88601a6a8a5d60c62d3c29e804d6a
vamshi-krishna-alluri <allurivamshi143@gmail.com>
2024-11-27 05:39:07 +0000

    lab submission

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express')
 const api = require('./api')
 const middleware = require('./middleware')
 const bodyParser = require('body-parser')
-
-
 // Set the port
 const port = process.env.PORT || 3000
 // Boot the app
@@ -19,6 +17,9 @@ app.get('/products/:id', api.getProduct)
 app.put('/products/:id', api.editProduct)
 app.delete('/products/:id', api.deleteProduct)
 app.post('/products', api.createProduct)
+app.get('/orders', api.listOrders)
+app.post('/orders/', api.createOrder)
+app.put('/orders/', api.editOrder)
+app.delete('/orders/', api.deleteOrder)
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
-
--- products.js
+++ products.js
@@ -1,8 +1,37 @@
 const fs = require('fs').promises
 const path = require('path')
+const cuid = require('cuid')
+
+const db = require('./db')
 
 const productsFile = path.join(__dirname, 'data/full-products.json')
 
+// Define our Product Model
+const Product = db.model('Product', {
+  _id: { type: String, default: cuid },
+  description: { type: String },
+  alt_description: { type: String },
+  likes: { type: Number, required: true },
+  urls: {
+    regular: { type: String, required: true },
+    small: { type: String, required: true },
+    thumb: { type: String, required: true },
+  },
+  links: {
+    self: { type: String, required: true },
+    html: { type: String, required: true },
+  },
+  user: {
+    id: { type: String, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String },
+    portfolio_url: { type: String },
+    username: { type: String, required: true },
+  },
+  tags: [{
+    title: { type: String, required: true },
+  }], 
+})
 /**
  * List products
  * @param {*} options 
@@ -17,11 +46,23 @@ async function list(options = {}) {
     .filter(product => {
       if (!tag) {
         return product
+  const query = tag ? {
+    tags: {
+      $elemMatch: {
+        title: tag
       }
 
       return product.tags.find(({ title }) => title == tag)
     })
     .slice(offset, offset + limit) // Slice the products
+    }
+  } : {}
+  const products = await Product.find(query)
+    .sort({ _id: 1 })
+    .skip(offset)
+    .limit(limit)
+
+  return products
 }
 
 /**
@@ -31,6 +72,9 @@ async function list(options = {}) {
  */
 async function get(id) {
   const products = JSON.parse(await fs.readFile(productsFile))
+  const product = await Product.findById(_id)
+  return product
+}
 
   // Loop through the products and return the product with the matching id
   for (let i = 0; i < products.length; i++) {
@@ -38,12 +82,32 @@ async function get(id) {
       return products[i]
     }
   }
+async function create (fields) {
+  const product = await new Product(fields).save()
+  return product
+}
+
+async function edit (_id, change) {
+  const product = await get(_id)
+
+  Object.keys(change).forEach(function (key) {
+    product[key] = change[key]
+  })
+
+  await product.save()
+
+  return product
+}
 
   // If no product is found, return null
   return null;
+async function destroy (_id) {
+  return await Product.deleteOne({_id})
 }
 
 module.exports = {
   list,
   get
+  get,
+  create
 }
